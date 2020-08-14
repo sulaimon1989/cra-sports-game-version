a6e1793c1c01f308fcb6c5495b47c51531631a72
sulaimon kadelu <sulaimonkadelu89@gmail.com>
2020-08-14 17:20:07 -0400

    cra version done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Game from "./components/game/Game";
+import raccoonspic from "./components/assets/images/raccoon.png";
+import squirrelspic from "./components/assets/images/squirrel.png";
+import houndspic from "./components/assets/images/hound.png";
+import bunniespic from "./components/assets/images/bunny.png";
 
-function App() {
+function App(props) {
+  const raccoons = {
+    name: "Russiaville Raccoons",
+    logoSrc: raccoonspic,
+  };
+
+  const squirrels = {
+    name: "Sheridan Squirrels",
+    logoSrc: squirrelspic,
+  };
+
+  const bunnies = {
+    name: "Burlington Bunnies",
+    logoSrc: bunniespic,
+  };
+
+  const hounds = {
+    name: "Hammond Hounds",
+    logoSrc: houndspic,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game
+        venue="Union 525 Gem"
+        homeTeam={squirrels}
+        visitingTeam={raccoons}
+      />
+      <Game venue="Sheridan Arena" homeTeam={bunnies} visitingTeam={hounds} />
     </div>
   );
 }
